3ca2002cc1e2f3c21eed6432edb0d2bee737d1fe
Khansagilani <115337402+Khansagilani@users.noreply.github.com>
2022-11-30 20:42:57 +0500

    admin files backup

---
+++ db.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql2");
+//--------------------------------------
+// Data base connection code
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'khansa1086',
+    database: 'WebProgramming'
+});
+
+con.connect((error) => {
+    if (error) {
+        console.warn('error' + JSON.stringify(error, undefined, 2))
+    }
+}
+);
+
+//optionalchecking
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+module.exports = sequelize;
---
+++ routes/index.routes.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const index = require("../controllers/index.controller")
+const admin = require("../controllers/admin.controller")
+const controller = require("../controllers/tours.controller");
+
+/* GET home page. */
+router.get('/', function (req, res) { res.render('home', { session: req.session }); });
+// Login
+router.get('/signin', (req, res) => { res.render('signin', { session: req.session }); });
+router.post('/signin', (req, res) => { index.signin(req, res) });
+router.get('/logout', (request, response) => {
+    request.session.destroy();
+    response.redirect("/");
+});
+// Register
+router.get('/addnewadmin', (req, res) => { res.render('addnewadmin') });
+router.post('/addnewadmin', index.signup);
+
+//input for admin
+router.get('/admininput', (req, res) => { res.render('/home') });
+router.post('/admininput', (req, res) => { admin.admininput(req, res) });
+//delete for admin
+router.get('/delete/:id', (req, res) => { admin.deleteadmin(res, resp) });
+//search 
+router.post('/search/:id', (req, res) => { admin.adminsearch(req, res) });
+//update 
+router.get('/update/:id', (req, res) => { admin.update(req, res) });
+router.post('/update/:id', (req, res) => { users.updated(req, res) });
+
+module.exports = router;
+
